43a95d05fd4644f48fc5b7c64fbab43597ef31af
Delveri Chick <chidelmun@delveris-air.lan>
2019-01-30 18:56:06 -0500

    Save changes

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'demo')));
+
+app.listen(5000, () => {
+    console.log('App running on port 5000....');
+})
